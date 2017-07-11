d726dbc23668a0c59f26cbde22cb3f378835fac8
韩勇 <hanyong@xinguangnet.com>
2017-07-11 17:17:36 +0800

    add config server

---
+++ config-server/src/main/java/com/ihanyong/spring/cloud/example/config/server/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.ihanyong.spring.cloud.example.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
