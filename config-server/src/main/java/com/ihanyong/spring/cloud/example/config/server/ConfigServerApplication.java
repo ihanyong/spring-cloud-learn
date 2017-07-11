e220ed79ee703851be4696a20fe4f4c269906e00
韩勇 <hanyong@xinguangnet.com>
2017-07-11 17:47:56 +0800

    test config

--- config-server/src/main/java/com/ihanyong/spring/cloud/example/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/ihanyong/spring/cloud/example/config/server/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.ihanyong.spring.cloud.example.config.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
+        System.out.println("hanyong");
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
